c0be58674d55d89e03d28557f9ed2eff2009c42b
Bhishan Mahat <bhishanmahat@Rukmanis-MacBook-Air.local>
2023-07-18 12:37:57 +0545

    Date Format Changed

--- lib/getFormattedDate.tsx
+++ lib/getFormattedDate.tsx
@@ -1,7 +1,7 @@
 const getFormattedDate = (dateUTC: string) => {
   const fdate = new Date(dateUTC);
-  let options = { year: "numeric", month: "long", day: "numeric" };
-  return new Intl.DateTimeFormat("en-US", options).format(fdate);
+  // let options = { year: "numeric", month: "long", day: "numeric" };
+  return new Intl.DateTimeFormat("en-US").format(fdate);
 };
 
 export default getFormattedDate;
